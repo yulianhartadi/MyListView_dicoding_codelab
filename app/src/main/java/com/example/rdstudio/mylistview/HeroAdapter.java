afb8cd899865ae2357e40db132ad0b7e7e5ac3ba
RawzaDigital <rawzadigital@gmail.com>
2019-08-23 10:40:43 +0700

    add Custom Adapter named Hero Adapter extends to BaseAdapter, add context and arraylist hero

--- app/src/main/java/com/example/rdstudio/mylistview/HeroAdapter.java
+++ app/src/main/java/com/example/rdstudio/mylistview/HeroAdapter.java
@@ -1,7 +1,17 @@
 package com.example.rdstudio.mylistview;
 
 
-public class HeroAdapter  {
+import android.content.Context;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+public class HeroAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<Hero> heroes;
+
+    
 
 
 }
