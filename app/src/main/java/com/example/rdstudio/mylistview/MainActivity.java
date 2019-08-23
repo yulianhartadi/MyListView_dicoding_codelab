b4e72baca81c5066d5a612d5f06e061e4d24afcb
RawzaDigital <rawzadigital@gmail.com>
2019-08-23 10:33:08 +0700

    add dummy ArrayList

--- app/src/main/java/com/example/rdstudio/mylistview/MainActivity.java
+++ app/src/main/java/com/example/rdstudio/mylistview/MainActivity.java
@@ -9,9 +9,16 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String[] dataName = {"Cut Nyak Dien", "Ki Hajar Dewantara", "Moh Yamin", "Patimura", "R A Kartini", "Sukarno"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.lv_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, dataName);
+        listView.setAdapter(adapter);
     }
 }
