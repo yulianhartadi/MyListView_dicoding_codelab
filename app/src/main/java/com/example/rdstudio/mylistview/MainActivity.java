cc86abfa4abcbf104b622bf25fe108906331ca7a
RawzaDigital <rawzadigital@gmail.com>
2019-08-23 10:56:41 +0700

    call Adapter to Main Activity

--- app/src/main/java/com/example/rdstudio/mylistview/MainActivity.java
+++ app/src/main/java/com/example/rdstudio/mylistview/MainActivity.java
@@ -9,16 +9,30 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String[] dataName = {"Cut Nyak Dien", "Ki Hajar Dewantara", "Moh Yamin", "Patimura", "R A Kartini", "Sukarno"};
+    // dummy data list view
+    //private String[] dataName = {"Cut Nyak Dien", "Ki Hajar Dewantara", "Moh Yamin", "Patimura", "R A Kartini", "Sukarno"};
+
+    // Call Adapter to mainView
+    private String[] dataName;
+    private String[] dataDescription;
+    private TypedArray dataPhoto;
+    private HeroAdapter heroAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
+        * dummy main view
         ListView listView = findViewById(R.id.lv_list);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, dataName);
-        listView.setAdapter(adapter);
+        listView.setAdapter(adapter);*/
+
+        heroAdapter = new HeroAdapter(this);
+        ListView listView = findViewById(R.id.lv_list);
+        listView.setAdapter(heroAdapter);
+
     }
 }
