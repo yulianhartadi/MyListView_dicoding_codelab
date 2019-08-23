9dc1315eca020044dbb041a15e1ef5ef94207e0f
RawzaDigital <rawzadigital@gmail.com>
2019-08-23 10:43:30 +0700

    implement methods

--- app/src/main/java/com/example/rdstudio/mylistview/HeroAdapter.java
+++ app/src/main/java/com/example/rdstudio/mylistview/HeroAdapter.java
@@ -2,6 +2,8 @@ package com.example.rdstudio.mylistview;
 
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
 import java.util.ArrayList;
@@ -18,5 +20,25 @@ public class HeroAdapter extends BaseAdapter {
     public HeroAdapter(ArrayList<Hero> heroes) {
         this.heroes = heroes;
         heroes = new ArrayList<>();
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
     }
 }
