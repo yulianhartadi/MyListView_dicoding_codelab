0238b911a902c094084a5245600f3cc0d8647f3e
RawzaDigital <rawzadigital@gmail.com>
2019-08-23 10:51:06 +0700

    add implementation method and get the text, description and photo

--- app/src/main/java/com/example/rdstudio/mylistview/HeroAdapter.java
+++ app/src/main/java/com/example/rdstudio/mylistview/HeroAdapter.java
@@ -2,9 +2,12 @@ package com.example.rdstudio.mylistview;
 
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -24,21 +27,47 @@ public class HeroAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return 0;
+        return heroes.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return heroes.get(i);
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
+        if (view == null){
+            view = LayoutInflater.from(context).inflate(R.layout.item_hero, viewGroup, false);
+        }
+
+        ViewHolder viewhHolder = new ViewHolder(view);
+        Hero hero = (Hero) getItem(i);
+
+
         return null;
+    }
+
+    private class ViewHolder{
+        private TextView txtName;
+        private TextView txtDescription;
+        private ImageView imgPhoto;
+
+        ViewHolder(View view){
+            txtName = view.findViewById(R.id.txt_name);
+            txtDescription = view.findViewById(R.id.txt_description);
+            imgPhoto = view.findViewById(R.id.img_photo);
+        }
+
+        void bind(Hero hero){
+            txtName.setText(hero.getName());
+            txtDescription.setText(hero.getDescription());
+            imgPhoto.setImageResource(hero.getPhoto());
+        }
     }
 }
