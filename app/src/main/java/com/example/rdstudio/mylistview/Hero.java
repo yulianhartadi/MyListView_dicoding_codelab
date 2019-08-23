85b280bd4546809954aeea279a6bc173ba29b35e
RawzaDigital <rawzadigital@gmail.com>
2019-08-23 10:35:36 +0700

    add new java class named Hero for Setter and Getter

--- app/src/main/java/com/example/rdstudio/mylistview/Hero.java
+++ app/src/main/java/com/example/rdstudio/mylistview/Hero.java
@@ -1,5 +1,33 @@
 package com.example.rdstudio.mylistview;
 
+// Add new java class for Setter and Getter named Hero
 public class Hero {
 
+    private int photo;
+    private String name;
+    private String Description;
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
 }
